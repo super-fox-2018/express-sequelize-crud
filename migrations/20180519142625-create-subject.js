5e63aec82daabfda1c6af25c06328bf76fee9c81
Dwiky Erlangga <dwikyerl@gmail.com>
2018-05-20 20:43:55 +0700

    Update subject and student migrations to not allow null value

--- migrations/20180519142625-create-subject.js
+++ migrations/20180519142625-create-subject.js
@@ -10,6 +10,7 @@ module.exports = {
       },
       subject_name: {
         type: Sequelize.STRING,
+        allowNull: false,
       },
       createdAt: {
         type: Sequelize.DATE,
--- migrations/20180519143216-create-student.js
+++ migrations/20180519143216-create-student.js
@@ -9,13 +9,16 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       first_name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       last_name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       createdAt: {
         type: Sequelize.DATE,
